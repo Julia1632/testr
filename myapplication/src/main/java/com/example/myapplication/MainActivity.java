6b2dfbf50b176403795efae8ea0b05bfb6947c97
Julia1632 <julia.zhurun6552817w@gmail.com>
2017-10-06 21:50:26 +0300

    main2

--- myapplication/src/main/java/com/example/myapplication/MainActivity.java
+++ myapplication/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // main 1
+        // main 2
     }
 }

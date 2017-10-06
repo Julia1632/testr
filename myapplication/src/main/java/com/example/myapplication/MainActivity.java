4e96537dae8940ded66b4597bc300a43f6978f9e
Julia1632 <julia.zhurun6552817w@gmail.com>
2017-10-06 23:04:44 +0300

    clear

--- myapplication/src/main/java/com/example/myapplication/MainActivity.java
+++ myapplication/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,7 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//Add 1
-        //Add 2
+
     }
 }

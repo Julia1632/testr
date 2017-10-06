d80a9b8a95464b586088a1677167139737f78f5e
Julia1632 <julia.zhurun6552817w@gmail.com>
2017-10-07 01:31:34 +0300

    bbbbb

--- myapplication/src/main/java/com/example/myapplication/MainActivity.java
+++ myapplication/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,8 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//Add feat main 1
-        // 2 b f
+
 
 
     }
